6bb00c782026aab29620ecc87382f5730d69d133
solovyovsdom <joelborofsky@mac.com>
2022-09-09 13:38:39 -0500

    finished first half of swiper functionality

---
+++ src/assets/js/data.js
@@ -0,0 +1,56 @@
+import images from './images'
+
+let reviews = [
+    {
+        name: "David",
+        position: "Director",
+        image: images.david,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        name: "Gus",
+        position: "Co-Director",
+        image: images.gus,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        name: "Andre",
+        position: "SAR Teams Lead",
+        image: images.andre,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        name: "Serhii",
+        position: "Supply Managerr",
+        image: images.serhii,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        name: "Roman",
+        position: "Dispatch Manager",
+        image: images.anon,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        name: "Anna",
+        position: "Coordinator",
+        image: images.anon,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        name: "Stewart",
+        position: "Fleet Manager",
+        image: images.stewart,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        name: "Sharon",
+        position: "Logistics",
+        image: images.sharon,
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    
+
+]
+
+export { reviews }
---
+++ src/assets/js/images.js
@@ -0,0 +1,19 @@
+import david from '../images/avatars/david.jpg'
+import gus from '../images/avatars/gus.jpg'
+import andre from '../images/avatars/andre.jpg'
+import serhii from '../images/avatars/serhii.jpg'
+import anon from '../images/avatars/anon.png'
+import stewart from '../images/avatars/stewart.jpg'
+import sharon from '../images/avatars/sharon.jpg'
+
+const images = {
+    david,
+    gus,
+    andre,
+    serhii,
+    anon,
+    stewart,
+    sharon
+}
+
+export default images
