265f0b06d44c6ab68380fb6127249e6c28481be4
solovyovsdom <joelborofsky@mac.com>
2022-09-09 17:21:45 -0500

    added donate and faq sections, need to style

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -111,3 +111,14 @@ reviews.map((reviews) => {
     `
     swiperContainer.innerHTML += template 
 })
+
+// FAQ
+
+const questions = [...document.querySelectorAll(".question")]
+questions.map((question) => {
+    let q_text = question.querySelector("h3")
+    question.addEventListener('click', () => {
+        questions.filter((q) => q !== question).map((q) => q.classList.remove("open"))
+        question.classList.toggle("open")
+    })
+})
