33a71f290ee161722f5f4eb1f46cacc6c073772f
solovyovsdom <joelborofsky@mac.com>
2022-09-09 22:21:16 -0500

    finished smooth scrolling

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,92 +1,195 @@
-import gsap from 'gsap'
-import Swiper, { Pagination, Navigation } from 'swiper'
-import { reviews } from './data'
+import gsap from 'gsap';
+import imagesLoaded from 'imagesLoaded';
+import Scrollbar, { ScrollbarPlugin } from 'smooth-scrollbar';
+import Swiper, { Pagination, Navigation } from 'swiper';
+import { reviews } from './data';
 
+class DisableScrollPlugin extends ScrollbarPlugin {
+  static pluginName = 'disableScroll';
 
+  static defaultOptions = {
+    direction: '',
+  };
 
-const bar = document.querySelector('.loading__bar--inner')
-const counterNumber = document.querySelector('.loading__counter--number')
+  transformDelta(delta) {
+    if (this.options.direction) {
+      delta[this.options.direction] = 0;
+    }
 
-let c = 0
+    return { ...delta };
+  }
+}
+
+// load the plugin
+Scrollbar.use(DisableScrollPlugin);
+
+class AnchorPlugin extends ScrollbarPlugin {
+  static pluginName = 'anchor';
+
+  onHashChange = () => {
+    this.jumpToHash(window.location.hash);
+  };
+
+  onClick = (event) => {
+    const { target } = event;
+
+    if (target.tagName !== 'A') {
+      return;
+    }
+
+    const hash = target.getAttribute('href');
+
+    if (!hash || hash.charAt(0) !== '#') {
+      return;
+    }
+
+    this.jumpToHash(hash);
+  };
+
+  jumpToHash = (hash) => {
+    const { scrollbar } = this;
+
+    if (!hash) {
+      return;
+    }
+
+    // reset scrollTop
+    scrollbar.containerEl.scrollTop = 0;
+
+    scrollbar.scrollIntoView(document.querySelector(hash));
+  };
+
+  onInit() {
+    this.jumpToHash(window.location.hash);
+
+    window.addEventListener('hashchange', this.onHashChange);
+
+    this.scrollbar.contentEl.addEventListener('click', this.onClick);
+  }
+
+  onDestory() {
+    window.removeEventListener('hashchange', this.onHashChange);
+
+    this.scrollbar.contentEl.removeEventListener('click', this.onClick);
+  }
+}
+
+// usage
+Scrollbar.use(AnchorPlugin);
+
+const bar = document.querySelector('.loading__bar--inner');
+const counterNumber = document.querySelector('.loading__counter--number');
+
+let c = 0;
 
 // LOADER
 
 let barInterval = setInterval(() => {
-    bar.style.width = c + '%'
-    counterNumber.innerText = c + '%'
-    c++
-    if (c === 101) {
-        clearInterval(barInterval)
-        gsap.to('.loading__bar', {
-            duration: 8,
-            rotate: "90deg",
-            left: '1000%'
-        })
-        gsap.to('.loading__text, .loading__counter', {
-            duration: .5,
-            opacity: 0,
-        })
-        gsap.to('.loading__box', {
-            duration: 1,
-            height: '500px',
-            borderRadius: '50%',
-        })
-        gsap.to('.loading__img', {
-            duration: 10,
-            opacity: 1,
-            rotate: '360deg',
-        })
-        gsap.to('.loading__box', {
-            delay: 2,
-            border: 'none'
-        })
-        gsap.to('.loading', {
-            delay: 2,
-            duration: 2,
-            // zIndex: 1,
-            background: 'transparent',
-            opacity: .5,
-        })
-        gsap.to('.loading__img', {
-            delay: 10,
-            // duration: 100,
-            rotate: '360deg',
-            display: 'none',
-        })
-    }
-
-}, 20)
+  bar.style.width = c + '%';
+  counterNumber.innerText = c + '%';
+  c++;
+  if (c === 101) {
+    clearInterval(barInterval);
+    gsap.to('.loading__bar', {
+      duration: 8,
+      rotate: '90deg',
+      left: '1000%',
+    });
+    gsap.to('.loading__text, .loading__counter', {
+      duration: 0.5,
+      opacity: 0,
+    });
+    gsap.to('.loading__box', {
+      duration: 1,
+      height: '500px',
+      borderRadius: '50%',
+    });
+    gsap.to('.loading__img', {
+      duration: 10,
+      opacity: 1,
+      rotate: '360deg',
+    });
+    gsap.to('.loading__box', {
+      delay: 2,
+      border: 'none',
+    });
+    imagesLoaded(document.querySelectorAll('img'), () => {
+      gsap.to('.loading', {
+        delay: 2,
+        duration: 2,
+        zIndex: 1,
+        background: 'transparent',
+        opacity: 0.5,
+      });
+      gsap.to('.loading__img', {
+        delay: 10,
+        duration: 100,
+        rotate: '360deg',
+      });
+      gsap.to('header', {
+        duration: 1,
+        delay: 2,
+        top: '0',
+      });
+      gsap.to('.socials', {
+        duration: 1,
+        delay: 2,
+        bottom: '10rem',
+      });
+      gsap.to('.scrollDown', {
+        duration: 1,
+        delay: 2.5,
+        bottom: '3rem',
+      });
+      gsap.to('.intro', {
+        duration: 1,
+        delay: 2,
+        left: '1rem',
+      });
+      let options = {
+        damping: 0.1,
+        plugins: {
+          disableScroll: {
+            direction: 'x',
+          },
+        },
+      };
+      let pageSmoothScroll = Scrollbar.init(document.body, options);
+      pageSmoothScroll.track.xAxis.element.remove();
+    }, 2000);
+  }
+}, 20);
 
 // SWIPER
-Swiper.use([Pagination, Navigation])
-var swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    // breakpoints: {
-    //     850:{
-    //         slidesPerView: 2,
-    //     },
-    //     1400:{
-    //         slidesPerView: 3,
-    //     },
-    //     1900:{
-    //         slidesPerView: 4,
-    //     }
-    // }
+Swiper.use([Pagination, Navigation]);
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // breakpoints: {
+  //     850:{
+  //         slidesPerView: 2,
+  //     },
+  //     1400:{
+  //         slidesPerView: 3,
+  //     },
+  //     1900:{
+  //         slidesPerView: 4,
+  //     }
+  // }
 });
 
-const swiperContainer = document.querySelector('.swiper-wrapper')
+const swiperContainer = document.querySelector('.swiper-wrapper');
 reviews.map((reviews) => {
-    let template = `
+  let template = `
          <div class="swiper-slide">
                         <div class="member">
                             <svg width="100%" height="100%" viewBox="0 0 33 27" fill="none"
@@ -108,17 +211,19 @@ reviews.map((reviews) => {
                             </div>
                         </div>
                     </div>
-    `
-    swiperContainer.innerHTML += template 
-})
+    `;
+  swiperContainer.innerHTML += template;
+});
 
 // FAQ
 
-const questions = [...document.querySelectorAll(".question")]
+const questions = [...document.querySelectorAll('.question')];
 questions.map((question) => {
-    let q_text = question.querySelector("h3")
-    question.addEventListener('click', () => {
-        questions.filter((q) => q !== question).map((q) => q.classList.remove("open"))
-        question.classList.toggle("open")
-    })
-})
+  let q_text = question.querySelector('h3');
+  question.addEventListener('click', () => {
+    questions
+      .filter((q) => q !== question)
+      .map((q) => q.classList.remove('open'));
+    question.classList.toggle('open');
+  });
+});
